8d63c0744d6b5aa155391d44d42cca5f08c59f63
Steven Gong <gonghaima@hotmail.com>
2024-01-26 17:18:30 +1100

    create customEvent & interface & initial class

--- ThermostatEventsApp/Program.cs
+++ ThermostatEventsApp/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ThermostatEventsApp;
+﻿using System.ComponentModel;
+
+namespace ThermostatEventsApp;
 
 class Program
 {
@@ -8,3 +10,65 @@ class Program
     }
 }
 
+public class HeatSensor : IHeatSensor
+{
+    event EventHandler<TemperatureEventArgs> IHeatSensor.TemperatureReachesEmergencyLevelEventHandler
+    {
+        add
+        {
+            throw new NotImplementedException();
+        }
+
+        remove
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    event EventHandler<TemperatureEventArgs> IHeatSensor.TemperatureReachesWarningLevelEventHandler
+    {
+        add
+        {
+            throw new NotImplementedException();
+        }
+
+        remove
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    event EventHandler<TemperatureEventArgs> IHeatSensor.TemperatureFallsBelowWarningLevelEventHandler
+    {
+        add
+        {
+            throw new NotImplementedException();
+        }
+
+        remove
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    void IHeatSensor.RunHeatSensor()
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public interface IHeatSensor
+{
+    event EventHandler<TemperatureEventArgs> TemperatureReachesEmergencyLevelEventHandler;
+    event EventHandler<TemperatureEventArgs> TemperatureReachesWarningLevelEventHandler;
+    event EventHandler<TemperatureEventArgs> TemperatureFallsBelowWarningLevelEventHandler;
+
+    void RunHeatSensor();
+}
+
+public class TemperatureEventArgs : EventArgs
+{
+    public double Temperature { get; set; }
+    public DateTime CurrentDateTime { get; set; }
+
+}
